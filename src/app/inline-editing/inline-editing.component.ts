4031271e908c5c8581ea815761e3a14d6ff41aec
Hai Long <hailongkc1203@gmail.com>
2025-09-05 09:40:59 +0700

    Inline Table Editing In Angular

--- src/app/inline-editing/inline-editing.component.ts
+++ src/app/inline-editing/inline-editing.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { NzDividerModule } from 'ng-zorro-antd/divider';
 import { NzTableModule } from 'ng-zorro-antd/table';
 
@@ -7,7 +8,7 @@ import { NzTableModule } from 'ng-zorro-antd/table';
   selector: 'app-inline-editing',
   templateUrl: './inline-editing.component.html',
   styleUrls: ['./inline-editing.component.css'],
-  imports: [NzDividerModule, NzTableModule],
+  imports: [NzDividerModule, NzTableModule, FormsModule],
 })
 export class InlineEditingComponent implements OnInit {
   postList: any[] = [];
@@ -26,5 +27,40 @@ export class InlineEditingComponent implements OnInit {
       .subscribe((res: any) => {
         this.postList = res;
       });
+  }
+
+  onEdit(record: any) {
+    this.removeEditable();
+    record.isEditable = true;
+  }
+
+  onSave(data: any) {
+    if (data.userId == '' || data.title == '' || data.body == '') {
+      alert('Provide all values');
+    } else {
+      console.log(data);
+    }
+  }
+  addNew() {
+    this.removeEditable();
+    const newObj = {
+      userId: '',
+      id: 0,
+      title: '',
+      body: '',
+      isEditable: true,
+    };
+    this.postList.unshift(newObj);
+  }
+  removeEditable() {
+    this.postList.forEach((element) => {
+      element.isEditable = false;
+    });
+  }
+  cancelRow(item: any) {
+    if (item.id === 0) {
+      this.postList.splice(0, 1);
+    }
+    item.isEditable = false;
   }
 }
