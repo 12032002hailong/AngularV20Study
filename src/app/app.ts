7de22c41df33d8578402b14a58992d9d96ae4390
Hai Long <hailongkc1203@gmail.com>
2025-09-06 14:04:49 +0700

    base Employee leave management app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { InlineEditingComponent } from './inline-editing/inline-editing.component';
 import { provideHttpClient } from '@angular/common/http';
+import { InlineEditingComponent } from './pages/inline-editing/inline-editing.component';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, InlineEditingComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
-
 })
 export class App {
   protected title = 'angularStudyV20';
